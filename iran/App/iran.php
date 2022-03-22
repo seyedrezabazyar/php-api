8ea0d7c391a80b4240e98ee66cd524831f2a4c6e
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-03-22 11:57:46 +0430

    Add request method + get list of cities based on province ID with Get method

--- iran/App/iran.php
+++ iran/App/iran.php
@@ -29,7 +29,7 @@ function getCities($data = null)
     global $pdo;
     $province_id = $data['province_id'] ?? null;
     $where = '';
-    if (!is_null($province_id) and is_int($province_id)) {
+    if (!is_null($province_id) and is_numeric($province_id)) {
         $where = "where province_id = {$province_id} ";
     }
     $sql = "select * from city $where";
--- iran/api/v1/cities/index.php
+++ iran/api/v1/cities/index.php
@@ -4,6 +4,29 @@ include_once '../../../loader.php';
 use App\Services\CityService;
 use App\Utilities\Response;
 
-$cs = new CityService();
-$result = $cs->getCities((object)[1,2,3,4,5,6,7]);
-Response::respondAndDie($result,Response::HTTP_OK);
+$request_method = $_SERVER['REQUEST_METHOD'];
+
+$request_body = json_decode(file_get_contents('php://input'),true);
+Response::respondAndDie($request_body);
+
+switch ($request_method){
+    case 'GET':
+        $province_id = $_GET['province_id'] ?? null;
+        $request_data = [
+            'province_id' => $province_id
+        ];
+        $response = getCities($request_data);
+        Response::respondAndDie($response,Response::HTTP_OK);
+ 
+    case 'POST':
+        Response::respondAndDie($_POST,Response::HTTP_OK);
+
+    case 'PUT':
+        Response::respondAndDie(['PUT Request'],Response::HTTP_OK);
+
+    case 'DELETE':
+        Response::respondAndDie(['DELETE Request'],Response::HTTP_OK);
+
+    default:
+        Response::respondAndDie(['invalid request method'],Response::HTTP_METHOD_NOT_ALLOWED);
+}
