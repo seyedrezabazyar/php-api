cacdbea7ac42fc01495ccca01ae0f3a5f97b4710
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-03-23 18:52:01 +0430

    Delete unnecessary files + transfer PDF file of project documentation to Iran folder

--- iran/App/Services/CityService.php
+++ iran/App/Services/CityService.php
@@ -15,4 +15,10 @@ class CityService
         $result = addCity($data);
         return $result;
     }
+
+    public function updateCityName($city_id, $name)
+    {
+        $result = changeCityName($city_id, $name);
+        return $result;
+    }
 }
